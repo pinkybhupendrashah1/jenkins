1c6498f9e932870994234d02eb87b726d9eb2d69
Pinky Shah <pinky.shah@CTPL.in>
2021-01-23 10:23:08 +0530

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+public static void main(String [] arz ){
+
+System.out.println("Hello World ");
+System.out.println("Hello World 1");
+
+System.out.println("Hello World 22");
+
+}
+
+}
